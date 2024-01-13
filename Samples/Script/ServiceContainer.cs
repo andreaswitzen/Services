16233e0e894a86b9b88c4c02b8241ae7edb4d78c
Andreas <andreaswitzen@gmail.com>
2024-01-14 00:48:13 +0100

    Break out ServiceContainer from ServiceLocator
    - Break out ServiceContainer scriptable object from ServiceLocator.
    - Make ServiceLocator static.
    
    This should fix a bug where Service instances weren't cleared when exiting play mode.

---
+++ Samples/Script/ServiceContainer.cs
@@ -0,0 +1,44 @@
+#nullable enable
+using UnityEditor;
+using UnityEngine;
+
+namespace Packages.com.andreaswitzen.services.Samples.Script
+{
+    [CreateAssetMenu(menuName = "ServiceContainer (Example)")]
+    public class ServiceContainer : ScriptableObject
+    {
+        public static ServiceContainer? Instance { get; set; }
+
+        public bool LoggingEnabled { get; set; }
+        public GameObject? ExampleSystemPrefab;
+
+        public void OnEnable()
+        {
+            TryRegister();
+        }
+
+        public void Awake()
+        {
+            TryRegister();
+        }
+
+        private void TryRegister()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                if (Instance.LoggingEnabled)
+                {
+                    Debug.Log("ServiceLocator: Instance registered.");
+                }
+            }
+            else if (!Instance.Equals(this))
+            {
+                Debug.LogError($"More than one service locator asset exists. " +
+                               $"The application might not behave predictably.\n" +
+                               $"Active instance path: {AssetDatabase.GetAssetPath(Instance.GetInstanceID())}\n" +
+                               $"Duplicate instance path: {AssetDatabase.GetAssetPath(GetInstanceID())}");
+            }
+        }
+    }
+}
