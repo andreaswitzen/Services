a148600f5161fce9d211b5916c57ada00fcf8062
Andreas <andreaswitzen@gmail.com>
2024-01-14 03:47:29 +0100

    Merge ServiceLocator and ServiceCollection into singleton

--- Samples/Script/ServiceLocator.cs
+++ Samples/Script/ServiceLocator.cs
@@ -6,10 +6,40 @@ using UnityEngine.Assertions;
 
 namespace Packages.com.andreaswitzen.services.Samples.Script
 {
-    public static class ServiceLocator
+    public class ServiceLocator : MonoBehaviour
     {
-        private static ServiceContainer Services => ServiceContainer.Instance!;
+        private static ServiceLocator? Instance { get; set; }
+
+        // Services
         private static ExampleServiceBase? _exampleService;
+
+        [field: SerializeField] private GameObject? ExampleServicePrefab { get; set; }
+
+        [field: SerializeField] private bool LoggingEnabled { get; set; }
+
+        public void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this);
+                if (Instance.LoggingEnabled)
+                {
+                    Debug.Log($"{GetType().Name}: Instance registered.");
+                }
+            }
+            else if (!Instance.Equals(this))
+            {
+                if (Instance.LoggingEnabled)
+                {
+                    Debug.Log($"{GetType().Name}: More than one service locator instance. Destroying.");
+                }
+
+                Destroy(this);
+            }
+
+            Assert.IsNotNull(Instance);
+        }
 
         public static ExampleServiceBase ExampleService
         {
@@ -20,15 +50,15 @@ namespace Packages.com.andreaswitzen.services.Samples.Script
                     return _exampleService;
                 }
 
-                // A ServiceLocator asset must exist. One can be created with the asset context menu
-                // (by right-clicking assets and selecting Create -> ServiceLocator).
-                Assert.IsNotNull(ServiceContainer.Instance);
-                _exampleService = CreateService<ExampleServiceBase, ExampleServiceFoo>(Services.ExampleSystemPrefab);
+                _exampleService = CreateService<ExampleServiceBase, ExampleServiceFoo>(Instance!.ExampleServicePrefab);
                 return _exampleService;
             }
         }
 
-
+        /*
+         * T = Base class.
+         * TU = Default implementation.
+         */
         private static T CreateService<T, TU>(GameObject? prefab = null) where T : Service where TU : T
         {
             // If a service prefab was provided, instantiate it.
@@ -37,17 +67,17 @@ namespace Packages.com.andreaswitzen.services.Samples.Script
                 var obj = Object.Instantiate(prefab);
                 if (obj.TryGetComponent(out T instantiatedService))
                 {
-                    if (ServiceContainer.Instance!.LoggingEnabled)
-                        Debug.Log($"ServiceLocator: Instantiated ExampleService of type: " +
+                    if (Instance!.LoggingEnabled)
+                        Debug.Log($"{Instance.GetType().Name}: Instantiated service of type: " +
                                   $"{instantiatedService!.GetType().Name}");
                     return instantiatedService;
                 }
             }
 
-            // Otherwise create a default service (one could also opt to create a dummy).
+            // Otherwise create default implementation (one could also opt to create a dummy).
             var createdService = Service.Create<TU>();
-            if (ServiceContainer.Instance!.LoggingEnabled)
-                Debug.Log($"ServiceLocator: Creating default ExampleService of type: " +
+            if (Instance!.LoggingEnabled)
+                Debug.Log($"{Instance.GetType().Name}: Creating default service of type: " +
                           $"{createdService.GetType().Name}.");
 
             return createdService;
